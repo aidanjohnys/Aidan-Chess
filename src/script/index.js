02fe5820a48dfd5e7bfaa5aaf10b208fe45ad97c
aidanjohnys <aidan.johnys@gmail.com>
2026-02-18 00:35:06 +0000

    add basic piece movement

--- src/script/index.js
+++ src/script/index.js
@@ -4,5 +4,39 @@ for (const piece of chessPieces) {
 }
 
 function clickedChessPiece(event) {
-    console.log(event)
+    const activeChessPieces = document.querySelectorAll('.active-chess-piece').length;
+
+    if (activeChessPieces < 1) {
+        event.currentTarget.classList.toggle('active-chess-piece');
+    }
+
+    else {
+        event.currentTarget.classList.remove('active-chess-piece')
+    }
+}
+
+const squares = document.querySelectorAll('.square');
+for (const square of squares) {
+    square.addEventListener('click', clickedSquare);
+}
+
+function clickedSquare(event) {
+    const chessPiece = document.querySelector('.active-chess-piece');
+
+    if (event.currentTarget.contains(chessPiece)) {
+        return;
+    }
+
+    if (chessPiece === null) {
+        return;
+    }
+
+    for (const child of event.currentTarget.children) {
+        if (child.classList.contains('chess-piece')) {
+            child.remove();
+        }
+    }
+
+    event.currentTarget.appendChild(chessPiece);
+    chessPiece.classList.remove('active-chess-piece');
 }
